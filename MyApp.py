d84bcb3fd3766f09111826f6f8bafe2f8091f99f
kiruukiran232 <106810619+kiruukiran232@users.noreply.github.com>
2024-01-08 11:35:11 +0530

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -1,1 +1,13 @@
-
+!pip install transformers
+from transformers import pipeline,AutoModelForSequenceClassification,AutoTokenizer
+model_name="nlptown/bert-base-multilingual-uncased-sentiment"
+model=AutoModelForSequenceClassification.from_pretrained(model_name)
+tokenizer=AutoTokenizer.from_pretrained(model_name)
+pipe=pipeline('sentiment-analysis')
+text="Chari ist ein gutes Mädchen"
+out=pipe(text)
+result=out[0] #Assuming you want the first result if multiple are returned
+sentiment=result["label"]
+score=round(result["score"],2)
+print(f"Sentiment: {sentiment}")
+print(score)
