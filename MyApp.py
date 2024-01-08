9ae878affa51a9bf45fcabfec21e2853e8b23679
kiruukiran232 <106810619+kiruukiran232@users.noreply.github.com>
2024-01-08 12:18:21 +0530

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -5,9 +5,9 @@ st.header("Types of Pets")
 col1, col2 = st.columns(2)
 with col1:
   st.subheader("Pilla Pichuka")
-  st.image("./IMG-20230405-WA0002.jpg", caption="Persian Cat", width=300,use_column_width=True)
+  st.image("./IMG-20230405-WA0002.jpg", caption="Persian Cat", width=300,height=500,use_column_width=True)
   st.write("This pilla pichuka is very rare piece in the world")
 with col2:
   st.subheader("Donkollu Pattevodu")
-  st.image("./Screenshot_20221127-143419_Call.jpg", caption="Ragdoll Cat", width=300,use_column_width=True)
+  st.image("./Screenshot_20221127-143419_Call.jpg", caption="Ragdoll Cat", width=300,height=500,use_column_width=True)
   st.write("This Donkollu pattevodu always catch Hens")
